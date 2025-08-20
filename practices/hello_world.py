3af9dc7449827108e542e5339b8f9b677190d15a
flumph3927 <levi.morris@ucas-edu.net>
2025-08-20 10:38:43 -0600

    Modified hello_world.py to be the advnced version

--- practices/hello_world.py
+++ practices/hello_world.py
@@ -1,4 +1,10 @@
 #LM 2nd Hello World
 
+letters=['a','b','c','d','e']
 name=input("What is your name? ")
-print("Hello",name)
+if name=="Levi" or name=="Ms. LaRose":
+    print("Hello, Admin",name)
+elif name in letters:
+    print("Welcome back, letter",name+'!')
+else:
+    print("Hello",name)
