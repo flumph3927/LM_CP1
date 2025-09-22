10a2f3fb186cdc8f4a8644bdbbb77cdc93a81126
flumph3927 <levi.morris@ucas-edu.net>
2025-09-22 10:35:12 -0600

    debugged paragraph splitter markov chains

--- self_projects/markov_chains.py
+++ self_projects/markov_chains.py
@@ -1,6 +1,6 @@
 import random, string
 
-base=[]
+base=['Write a program that asks a user for their username and their. If the username and the password match the ones given in the program it welcomes the user to the program. Otherwise it tells the user that their login credentials were invalid.','Have a selection of several different users with different passwords. Check to see if the username is one of the options, then check to see if the password matches for that given user. (I recommend using a list of users with the username and password set up as their own list)']
 source=[]
 data=[]
 starters=[]
@@ -10,6 +10,9 @@ for p in base:
     bases=p.split('.')
     for h in bases:
         source.append(h.strip())
+for m in source:
+    if m.strip()=='':
+        source.remove(m)
 index=-1
 for i in source:
     translator = str.maketrans('', '', (string.punctuation+'0123456789'))
