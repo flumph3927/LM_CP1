ecb92f00b544f6da6cf944ab4a7c86d4541a70c5
flumph3927 <levi.morris@ucas-edu.net>
2025-10-03 10:50:48 -0600

    rps

---
+++ practices/rock_paper_scissors.py
@@ -0,0 +1,7 @@
+#LM 2nd Rock Paper Scissors
+import random
+score=[0,0]
+while True:
+    print('The round has begun.')
+    print('What do you choose:\n1. Rock\n2. Paper\n3. Scissors\n4. Quit')
+    choice=input()
