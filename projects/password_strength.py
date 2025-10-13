1da10b9583fc5ee16bb3b563666ca1805fe3fd97
flumph3927 <levi.morris@ucas-edu.net>
2025-10-13 10:18:23 -0600

    PASSWORD

--- projects/password_strength.py
+++ projects/password_strength.py
@@ -1,4 +1,4 @@
-#LM 2nd Password strength checker
+#LM 2nd Password Strength Checker
 '''Psuedocode:
 set PASSWORD to user input
 set SCORE to 0
@@ -16,16 +16,39 @@ loop through characters in PASSWORD
         add one to score
         end loop
 loop through characters in PASSWORD
-    if character in password is in !@#$%^&*()_+-=[]{}|;:,.<>?
+    if character in password is in !@#$%^&*()_+-=[]{}|;:,.<>?/\
         add one to score
         end loop
 if SCORE is 5, display Very Strong Password
 else if SCORE is 4, display Strong Password
 else if SCORE is 3, display Weak Password
-else if SCORE is 0, display This actually might be good???
 else display Very Weak Password
 '''
-word=input('Enter password')
+word=input('Enter password: ')
 score=0
 if len(word)>7:
     score+=1
+for i in word:
+    if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+        score+=1
+        break
+for i in word:
+    if i in 'qwertyuiopasdfghjklzxcvbnm':
+        score+=1
+        break
+for i in word:
+    if i in '1234567890':
+        score+=1
+        break
+for i in word:
+    if i in '`~!@#$%^&*()_+-=[]{}|\\;:\'",.<>?/':
+        score+=1
+        break
+if score==5:
+    print('Very Strong Password')
+elif score==4:
+    print('Strong Password')
+elif score==3:
+    print('Weak Password')
+else:
+    print('Very Weak Password')
