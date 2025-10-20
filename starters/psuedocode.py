8e2cf19fd4257c0036ef32d26747629fc5105618
flumph3927 <levi.morris@ucas-edu.net>
2025-10-20 10:50:57 -0600

    goo

--- starters/psuedocode.py
+++ starters/psuedocode.py
@@ -4,12 +4,12 @@ bord=[1,2,3,4,5,6,7,8,9]
 while True:
     if input('Do you want to quit(y to quit)(anything else to continue)\n')=='y':
         break
-    print(bord[0],bord[1]+bord[2])
-    print(bord[3],bord[4]+bord[5])
-    print(bord[6],bord[7]+bord[8])
+    print(bord[0],bord[1],bord[2])
+    print(bord[3],bord[4],bord[5])
+    print(bord[6],bord[7],bord[8])
     player=input('Enter a square from 1-9\n')
     comp=random.randint(0,8)
-    bord[int(player)] = 'o'
+    bord[int(player)-1] = 'o'
     bord[comp] = 'x'
 
     if bord[0] == "x" and bord[1] == "x" and bord[2] == "x":
@@ -59,6 +59,6 @@ while True:
     elif bord[1] == "x" and bord[4] == "x" and bord[7] == "x":
         print("computer won")
         break
-    elif bord[2] == "x" and bord[5] == "x" and bord[8] == "x":
+    elif bord[0] == "x" and bord[5] == "x" and bord[8] == "x":
         print("computer won")
         break
