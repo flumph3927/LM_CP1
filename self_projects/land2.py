117019527da769516045e22086381c305fe97a16
flumph3927 <levi.morris@ucas-edu.net>
2025-10-27 10:51:02 -0600

    land

--- self_projects/land2.py
+++ self_projects/land2.py
@@ -22,6 +22,32 @@ while not dne:
         for x in i:
             if x == '':
                 dne=False
+
+
+mp=[]
+for i in range(y):
+    mp.append([])
+    for x in range(z):
+        mp[i].append('')
+
+dne=False
+while not dne:
+    for i in range(y):
+        for x in range(z):
+            tle=[i,x]
+            ps=['-','-','~','~','~','~','^','+','+']
+            for j in [[tle[0],tle[1]+1],[tle[0]-1,tle[1]],[tle[0]+1,tle[1]],[tle[0],tle[1]-1]]:
+                if j[0]<(y-1) and j[0]>-1 and j[1]>-1 and j[1]<(z-1):
+                    if mp[j[0]][j[1]] != '':
+                        for a in range(100):
+                            ps.append(mp[j[0]][j[1]])
+            mp[tle[0]][tle[1]]=random.choice(ps)
+    dne=True
+    for i in mp:
+        for x in i:
+            if x == '':
+                dne=False
+
 cds={'~':'\033[104m','-':'\033[43m','^':'\033[1m\033[47m','+':'\033[102m'}
 for i in mp:
     for x in i:
