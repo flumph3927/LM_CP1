46dd198424787668683722853554ad49a1f3cad8
flumph3927 <levi.morris@ucas-edu.net>
2025-11-10 10:50:22 -0700

    order

---
+++ practices/order.py
@@ -0,0 +1,23 @@
+#LM 2nd Order Up
+
+#set MENU to dictionary containing dictionaries of other types
+#set OUT to empty string
+#loop through dictionary
+#   add name of sub-dictionary to OUT
+#   loop through sub-dictionary
+#       add item and price to OUT
+#display welcome
+#display OUT
+#set DRINK to user input
+#set MAIN to user input
+#set SIDES to user inputs
+#add prices of DRINK, MAIN and SIDES to new variable PRICE
+#display PRICE
+#set TOTAL to PRICE and tax
+#displat TOTAL
+menu={
+    'drink': {
+        'water': 2.35,
+        'mud': 1.23
+    }
+}
