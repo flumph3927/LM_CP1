edde9ce39ab8466a729339ff1717dcf4414c819c
flumph3927 <levi.morris@ucas-edu.net>
2025-11-17 10:50:04 -0700

    calcium, for some reason

---
+++ practices/flexible_calculator.py
@@ -0,0 +1,22 @@
+#LM 2nd Flexible Calculator
+'''
+define function calculate, get OPERATION and NUMBERS
+    if OPERATION is sum
+        return NUMBERS added together
+    else if OPERATION is average
+        return NUMBERS added together, divided by number of NUMBERS
+    else if OPERATION is max
+        return largest number in NUMBERS
+    else if OPERATION is min
+        return smallest number in NUMBERS
+    else if OPERATION is product
+        return NUMBERS multiplied together
+define function check, get INPUT
+    try to make INPUT a float,
+    if error, return False
+    else, return float INPUT
+display enter numbers, done to stap entering numbers
+loop while INPUT not done:
+    set INPUT to user input
+    
+'''
