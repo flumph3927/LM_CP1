332d2a3906bc25851094cf9a054c496efd0e50f4
flumph3927 <levi.morris@ucas-edu.net>
2025-11-19 10:50:13 -0700

    code golf but better

--- practices/squares.py
+++ practices/squares.py
@@ -1,4 +1,4 @@
 #LM 2nd Squared Numbers
 
 #loop through number list using index and value, every iteration print the value and then the index of a squaring function run on every item in the list
-for i,v in enumerate([3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]): print(f'{v} squared is {list(map(lambda num: num*num, [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]))[i]}')
+for i,v in enumerate([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]): print(f'{v} squared is {list(map(lambda num: num*num, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]))[i]}')
