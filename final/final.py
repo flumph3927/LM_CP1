be4a8db531ae25bf33c8eb0ed7073f3db359da28
flumph3927 <levi.morris@ucas-edu.net>
2025-12-05 10:50:32 -0700

    final

---
+++ final/final.py
@@ -0,0 +1,29 @@
+#LM 2nd Final Project
+
+base=[[[[[{'enemies':0,'shop':0,'boss':'','passage':[]},{'enemies':0,'shop':0,'boss':'','passage':[]}],
+         [{'enemies':0,'shop':0,'boss':'','passage':[]},{'enemies':0,'shop':0,'boss':'','passage':[2,[0,0],1]}]],
+
+        [[{'enemies':0,'shop':0,'boss':'','passage':[1,[1,1],2]},{'enemies':0,'shop':0,'boss':'','passage':[3,[0,0],2]}],
+         [{'enemies':0,'shop':0,'boss':'','passage':[4,[0,1],2]},{'enemies':0,'shop':1,'boss':'','passage':[]}]],
+
+        [[{'enemies':1,'shop':0,'boss':'','passage':[2,[0,1],3]},{'enemies':2,'shop':0,'boss':'','passage':[7,[1,0],3]}],
+         [{'enemies':1,'shop':0,'boss':'','passage':[5,[0,1],3]},{'enemies':0,'shop':0,'boss':[BOSS IS HERE],'passage':[6,[0,1],3]}]]],
+
+        [[[{},{}],
+          [{},{}]],
+
+         [[{},{}],
+          [{},{}]],
+
+         [[{},{}],
+          [{},{}]]],
+
+         [[[{},{}],
+           [{},{}]],
+
+          [[{},{}],
+           [{},{}]],
+
+          [[{},{}],
+           [{},{}]]]],
+           {10:{'Daggers':[8,'spd']},10:{'Greatsword':[8,'str']},10:{'spell':[8,'mag']}},[]]#final boss last item
